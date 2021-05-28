8e311896366599597bb030b33c0bc0de63b877fa
DanieleLena <daniele.lena@yahoo.it>
2021-05-28 16:15:36 +0200

    final commit

--- app.js
+++ app.js
@@ -1,19 +1,51 @@
+//MOBILE-MENU
+
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".mobile-menu");
+const links = document.querySelectorAll(".mobile-link");
+const hamburgerIcon = document.getElementById("#hamburger");
+const crossIcon = document.getElementById("#cross");
+
+hamburgerMenu.addEventListener("click", () => {
+  if (hamburgerMenu.classList.contains("hamburger-close")) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    closeMenu();
+  });
+});
+
+const openMenu = () => {
+  hamburgerMenu.classList.toggle("hamburger-close");
+  menu.style.transition = "1.5s max-height ease-out";
+  menu.classList.add("mobile-menu-open");
+  hamburgerIcon.classList.toggle("hide-icon");
+  crossIcon.classList.toggle("hide-icon");
+};
+const closeMenu = () => {
+  hamburgerMenu.classList.toggle("hamburger-close");
+  menu.style.transition = "none";
+  menu.classList.remove("mobile-menu-open");
+  hamburgerIcon.classList.toggle("hide-icon");
+  crossIcon.classList.toggle("hide-icon");
+};
 
 // THR TOGGLE FAQ
 
 const faqs = document.querySelectorAll(".question");
 
 faqs.forEach((faq) => {
-
   faq.addEventListener("click", () => {
-
     faqs.forEach((faq2) => {
-        
       if (faq2 !== faq) {
         faq2.classList.remove("question-active");
       }
     });
-    // faq.style.maxHeight = "100%";
     faq.classList.toggle("question-active");
   });
 });
@@ -27,7 +59,6 @@ faqsBtn.forEach((btn) => {
         btn2.classList.remove("btn-active");
       }
     });
-    // faq.style.maxHeight = "100%";
     btn.classList.toggle("btn-active");
   });
 });
